3ee1bf856b41c52c44fab36b64ec89ad935b8ae1
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-12-08 20:42:01 -0300

    Variables, Constantes, Zero Values + Ejercicio

--- src/main.go
+++ src/main.go
@@ -3,5 +3,33 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hola Mundo desde Platzi")
+
+	//Constantes
+	const pi float64 = 3.14
+	const pi2 = 3.14
+
+	fmt.Println("Pi", pi)
+	fmt.Println("Pi2", pi2)
+
+	// Variables
+	base := 12
+	var altura int = 14
+	var area int
+
+	fmt.Println(base, altura, area)
+
+	// Zero Values
+	var a int
+	var b float64
+	var c string
+	var d bool
+
+	fmt.Println(a, b, c, d)
+
+	// Area Cuadrado BASE * ALTURA
+
+	const baseCuadrado = 10
+	areaCuadrado := baseCuadrado * baseCuadrado
+	fmt.Println(areaCuadrado)
+
 }
