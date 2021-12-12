ca3eda3cdcafe8a343d005fee0c704d63f6c11ee
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-12-12 11:17:40 -0300

    Arrasy & Slice

--- src/main.go
+++ src/main.go
@@ -3,27 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	// Defer: deja para ultimo momento, ejecuta antes de terminar el programa
-	// se usa por ejemplo, para cerrar una conexion con una BD, un archivo
-	// o para cerrar una conexion de red
 
-	defer fmt.Println("Hola")
-	fmt.Println("Mundo")
+	//Array
+	var array [4]int
+	array[0] = 1
+	array[1] = 2
+	fmt.Println(array, len(array), cap(array))
 
-	// Continue y break
-	for i := 0; i < 10; i++ {
+	//Slice
+	slice := []int{0, 1, 2, 3, 4, 5, 6}
+	fmt.Println("Slice: ", slice, len(slice), cap(slice))
 
-		// Continue
-		if i == 2 {
-			fmt.Println("es 2")
-			continue // el Println de abajo no se ejecuta, vuelve al for
-		}
+	//Metodos en Slice
+	fmt.Println(slice[0])
+	fmt.Println(slice[0:3])
+	fmt.Println(slice[2:4])
+	fmt.Println(slice[4:])
 
-		fmt.Println(i)
+	//* append
 
-		// Break
-		if i == 5 {
-			break
-		}
-	}
+	slice = append(slice, 25)
+	fmt.Println(slice)
+
+	// Add new list
+	newSlice := []int{8, 9, 10}
+	slice = append(slice, newSlice...)
+	fmt.Println(slice)
+
 }
