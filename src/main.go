c91cb60ba694716b8fb107b5756eb5e5fc585e3f
Juan Cruz Bidegain <juancruzbidegain@gmail.com>
2021-12-12 12:03:26 -0300

    Maps

--- src/main.go
+++ src/main.go
@@ -1,38 +1,29 @@
 package main
 
-import (
-	"fmt"
-)
-
-func isPalindromo(text string) {
-	var textReverse string
-	for i := len(text) - 1; i >= 0; i-- {
-		fmt.Println(string(text[i]))
-		textReverse += string(text[i])
-	}
-
-	fmt.Println(text, textReverse)
-
-	if text == textReverse {
-		fmt.Println("Es palindromo")
-	} else {
-		fmt.Println("No es un palindromo...")
-	}
-}
+import "fmt"
 
 func main() {
 
-	// slice := []string{"Hola", "Que", "Hace"}
-	// for i, value := range slice {
-	// 	fmt.Println(i, value)
+	m := make(map[string]int)
+
+	m["Jose"] = 14
+	m["Pepito"] = 20
+
+	fmt.Println(m)
+
+	// llave valor del map
+
+	for i, value := range m {
+		fmt.Println(i, value)
+	}
+
+	// value := m["1Jose"]
+	// if value == 0 {
+	// 	fmt.Println("No se encontro la llave solicitada")
+	// } else {
+	// 	fmt.Println(value)
 	// }
 
-	// for _, x := range slice {
-	// 	fmt.Println(x)
-	// }
-
-	// Funcion para un palindromo
-
-	isPalindromo("amor a roma")
-
+	value, ok := m["Jose"]
+	fmt.Println(value, ok)
 }
